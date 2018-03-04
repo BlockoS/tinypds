7a2f70fe35798e833124935c6a188a77aceec16a
mooz <mooz@blockos.org>
2018-03-04 21:50:02 +0100

    Fixed PDS parser unit test.

--- test/PDS_parse.c
+++ test/PDS_parse.c
@@ -1,34 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <errno.h>
-#include <tinypds.h>
-
-#include "test.h"
-#include "utils.h"
 
 #define TINY_PDS_IMPL
 #include <tinypds.h>
 
-typedef struct
-{
+typedef struct {
     int depth;
 } user_data_t;
 
-void print_tab(int depth)
-{
+void dummy_error(const PDS_error_description *desc, void *unused) {
+    (void)unused;
+    fprintf(stderr, "line %d:%d: %s\n", desc->number, desc->position, desc->msg);
+}
+
+void print_tab(int depth) {
     int i;
-    for(i=0; i<depth; i++)
-    {
+    for(i=0; i<depth; i++) {
         printf("    ");
     }
 }
-int dummy_scalar(const PDS_scalar *scalar, void *user_data)
-{
+
+void print_string(const char *first, const char *last) {
+    for(; first<=last; first++) {                                                                                                         
+        fputc(*first, stdout);                                                                                
+    }                                                                                                         
+}                                                                                                             
+                                                                                                              
+void print_scalar(const PDS_scalar *scalar) {
+    switch(scalar->type) {
+        case PDS_INTEGER_VALUE:                                                                               
+            printf("%ld ", scalar->integer.value);
+            if(scalar->integer.unit.first && scalar->integer.unit.last) {
+                print_string(scalar->integer.unit.first, scalar->integer.unit.last);
+            }                                                          
+            break;                                                                                            
+        case PDS_REAL_VALUE:                                                                                  
+            printf("%f ", scalar->real.value);                                                                
+            if(scalar->real.unit.first && scalar->real.unit.last) {
+                print_string(scalar->real.unit.first, scalar->real.unit.last);
+            }                                                          
+            break;                                                                                            
+        case PDS_DATE_TIME_VALUE:                                                                             
+            printf("%04d-%02d-%02d ", scalar->date_time.year, scalar->date_time.month, scalar->date_time.day);
+            printf("%02d:%02d:%02d:%02d ", scalar->date_time.hour, scalar->date_time.minute,
+                                           scalar->date_time.second, scalar->date_time.microsecond);
+            if(PDS_ZONED_TIME == scalar->date_time.time_type) {
+                printf("%02d:%02d ", scalar->date_time.hour_offset, scalar->date_time.minute_offset);
+            }
+            else if(PDS_UTC_TIME == scalar->date_time.time_type) {
+                printf("UTC");
+            }
+            break;                                                                                            
+        case PDS_TEXT_STRING_VALUE:                                                                           
+            printf("\"");                                                                                 
+            print_string(scalar->text.first, scalar->text.last);                                              
+            printf("\" ");                                                                                 
+            break;                                                                                            
+        case PDS_SYMBOLIC_VALUE:                                                                              
+            printf("\'");                                                                                 
+            print_string(scalar->text.first, scalar->text.last);                                              
+            printf("\' ");                                                                                 
+            break;                                                                                            
+        case PDS_IDENTIFIER_VALUE:                                                                            
+            print_string(scalar->identifier.first, scalar->identifier.last);                                  
+            break;                                                                                            
+    }                                                                                                         
+    printf("\n");
+}
+
+int dummy_scalar(const PDS_scalar *scalar, void *user_data) {
     (void)user_data;
     print_scalar(scalar);
     return 1;
 }
 
-int dummy_attribute_begin(const char *first, const char *last, void *user_data)
-{
+int dummy_attribute_begin(const char *first, const char *last, void *user_data) {
     user_data_t *data = (user_data_t*)user_data;
     print_tab(data->depth);
     printf("[attribute] ");
@@ -37,16 +85,14 @@ int dummy_attribute_begin(const char *first, const char *last, void *user_data)
     return 1;
 }
 
-int dummy_attribute_end(const char *first, const char *last, void *user_data)
-{
+int dummy_attribute_end(const char *first, const char *last, void *user_data) {
     (void)first;
     (void)last;
     (void)user_data;
     return 1;
 }
 
-int dummy_pointer_begin(const char *first, const char *last, void *user_data)
-{
+int dummy_pointer_begin(const char *first, const char *last, void *user_data) {
     user_data_t *data = (user_data_t*)user_data;
     print_tab(data->depth);
     printf("[pointer  ] ");
@@ -55,46 +101,44 @@ int dummy_pointer_begin(const char *first, const char *last, void *user_data)
     return 1;
 }
 
-int dummy_pointer_end(const char *first, const char *last, void *user_data)
-{
+int dummy_pointer_end(const char *first, const char *last, void *user_data) {
     (void)first;
     (void)last;
     (void)user_data;
     return 1;
 }
 
-int dummy_set_begin(void *user_data)
-{
+int dummy_set_begin(void *user_data) {
     user_data_t *data = (user_data_t*)user_data;
     data->depth++;
     printf(" {\n");
     return 1;
 }
-int dummy_set_end(void *user_data)
-{
+
+int dummy_set_end(void *user_data) {
     user_data_t *data = (user_data_t*)user_data;
     data->depth--;
     print_tab(data->depth);
     printf("}\n");
     return 1;
 }
-int dummy_sequence_begin(void *user_data)
-{
+
+int dummy_sequence_begin(void *user_data) {
     user_data_t *data = (user_data_t*)user_data;
     data->depth++;
     printf("(\n");
     return 1;
 }
-int dummy_sequence_end(void *user_data)
-{
+
+int dummy_sequence_end(void *user_data) {
     user_data_t *data = (user_data_t*)user_data;
     data->depth--;
     print_tab(data->depth);
     printf(")\n");
     return 1;
 }
-int dummy_object_begin(const char *first, const char *last, void *user_data)
-{
+
+int dummy_object_begin(const char *first, const char *last, void *user_data) {
     user_data_t *data = (user_data_t*)user_data;
     print_tab(data->depth);
     printf("[object   ] ");
@@ -103,28 +147,8 @@ int dummy_object_begin(const char *first, const char *last, void *user_data)
     data->depth++;
     return 1;
 }
-int dummy_object_end(const char *first, const char *last, void *user_data)
-{
-    (void)first;
-    (void)last;
-    user_data_t *data = (user_data_t*)user_data;
-    data->depth--;
-    print_tab(data->depth);
-    printf("]\n");
-    return 1;
-}
-int dummy_group_begin(const char *first, const char *last, void *user_data)
-{
-    user_data_t *data = (user_data_t*)user_data;
-    print_tab(data->depth);
-    printf("[group    ] ");
-    print_string(first, last);
-    printf(" = [\n");
-    data->depth++;
-    return 1;
-}
-int dummy_group_end(const char *first, const char *last, void *user_data)
-{
+
+int dummy_object_end(const char *first, const char *last, void *user_data) {
     (void)first;
     (void)last;
     user_data_t *data = (user_data_t*)user_data;
@@ -134,8 +158,27 @@ int dummy_group_end(const char *first, const char *last, void *user_data)
     return 1;
 }
 
-int main(int argc, const char* argv[])
-{
+int dummy_group_begin(const char *first, const char *last, void *user_data) {
+    user_data_t *data = (user_data_t*)user_data;
+    print_tab(data->depth);
+    printf("[group    ] ");
+    print_string(first, last);
+    printf(" = [\n");
+    data->depth++;
+    return 1;
+}
+
+int dummy_group_end(const char *first, const char *last, void *user_data) {
+    (void)first;
+    (void)last;
+    user_data_t *data = (user_data_t*)user_data;
+    data->depth--;
+    print_tab(data->depth);
+    printf("]\n");
+    return 1;
+}
+
+int main(int argc, const char* argv[]) {
     PDS_callbacks callbacks;
 
     FILE *in;
